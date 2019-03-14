14d3939690682752fa7cb9ba77ca63c1905a8275
Unknown <TAdcox009@west-mec.org>
2019-03-13 18:14:45 -0700

    commit 2
    Added a variable that stores an object collection for the stars, added a for loop that loops through the collection for each star.

--- bw_review.js
+++ bw_review.js
@@ -48,15 +48,49 @@ window.onload = init;
 
 // 5. Create the init() function. The purpose of the function is to define the event listeners used in the page. Add the following commands to the function:
 function init() {
-      var spa
+
+      // a. Declare the stars variable that stores an object collection of the reviewing stars, referenced by the span#stars img selector.
+      var stars = document.querySelectorAll("span#stars");
+
+      // b. Loop through the star collection and for each star image in the collection change the cursor style to pointer and add an event listener to run the lightStars() function in response to the mouseenter event occurring over each star image.
+      for (var i = 0; i < stars.length; i++) {
+            stars[i].style.cursor = "pointer";
+            document.addEventListener("mouseenter", lightStars);
+      }
+
+      // c. After the for loop, add an event listener to the comment text area box that runs the update- Count() function in response to the keyup event.
+      document.addEventListener("keyup", updateCount);
 }
 
-// a. Declare the stars variable that stores an object collection of the reviewing stars, referenced by the span#stars img selector.
-// b. Loop through the star collection and for each star image in the collection change the cursor style to pointer and add an event listener to run the lightStars() function in response to the mouseenter event occurring over each star image.
-// c. After the for loop, add an event listener to the comment text area box that runs the update- Count() function in response to the keyup event.
+// 6. Create the lightstars() function.The purpose of this function is to color a star when the user moves the mouse pointer over a star image in order to reflect the user’s rating of the book. Add the following commands to the function:
+function lightStars(e) {
+
+      // a. Daniel has stored the rating value of each star image in the img element’ s alt attribute. Store the value of the alt attribute of the target of the event object in the starNumber variable.
+      var starNumber = e.target.alt;
 
 
+      // b. Declare the stars variable containing the object collection referenced by the selector
+      // span# stars img.
+      var stars = document.querySelectorAll("span#stars");
 
+      // c. Loop through the stars collection with an index ranging from 0 up to less than the value of the starNumber variable.Light every star in the collection by changing the src attribute of the star image to the bw_star2.png image file.
+      for (var i = 0; i < array.length; i++) {
+
+      }
+
+      // d. After the for loop, create another loop that loops through the stars collection with the index ranging from the value of the starNumber variable to less than 5. Unlight every star in this col - lection by changing the src attribute of the star image to the bw_star.png image file.
+
+
+      // e. Change the value of the input box with the id attribute“ rating” to starNumber stars, where starNumber is the value of the starNumber variable.
+
+
+      // f. When the mouse pointer moves off a star image, the lit stars should be unlit.Add an event listener to the target of the event object that runs the turnOffStars() function in response to the mouseleave event.
+
+
+      // g. If the user clicks the star image, the selected rating should be set, which means moving the mouse pointer off the star should not remove the rating.Add an event listener for the target of the event object that runs an anonymous function removing the turnOffStars() function from the mouseleave event.
+
+
+}
 
 
 function countCharacters(textStr) {
