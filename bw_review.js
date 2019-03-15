1b896e3c54b3441f2478b2d5a130cdd358f5a677
Unknown <TAdcox009@west-mec.org>
2019-03-15 01:24:53 -0700

    Final commit
    Added comments, added event listeners when the user hovers over a star, removed event listeners when the user isn't hovering over a star, made a function to unlight stars, a function to count the words for the comment text area box. Final commit, ready for review.

--- bw_review.js
+++ bw_review.js
@@ -38,62 +38,87 @@
 
 
 
-
-
-
-
 /*=================================================================*/
 // Runs the init() function when the page is loaded by the browser.
 window.onload = init;
 
-// 5. Create the init() function. The purpose of the function is to define the event listeners used in the page. Add the following commands to the function:
+// Init() function, defines the event listeners used in the page.
+// Stars variable that stores the review stars.
+// For every star image its going to change the cursor style to a pointer and adds an event listener to run the lightStars() function in response to the mouseenter event when hovering over a star.
+// An event listener that runs the updateCount() function in response to the keyup event.
 function init() {
+      
+      var stars = document.querySelectorAll("span#stars img");
 
-      // a. Declare the stars variable that stores an object collection of the reviewing stars, referenced by the span#stars img selector.
-      var stars = document.querySelectorAll("span#stars");
-
-      // b. Loop through the star collection and for each star image in the collection change the cursor style to pointer and add an event listener to run the lightStars() function in response to the mouseenter event occurring over each star image.
       for (var i = 0; i < stars.length; i++) {
             stars[i].style.cursor = "pointer";
-            document.addEventListener("mouseenter", lightStars);
+            stars[i].addEventListener("mouseenter", lightStars);
       }
 
-      // c. After the for loop, add an event listener to the comment text area box that runs the update- Count() function in response to the keyup event.
-      document.addEventListener("keyup", updateCount);
+      document.getElementById("comment").addEventListener("keyup", updateCount);
 }
 
-// 6. Create the lightstars() function.The purpose of this function is to color a star when the user moves the mouse pointer over a star image in order to reflect the user’s rating of the book. Add the following commands to the function:
+// Function will color a star when the user moves the mouse pointer over a star image for the user’s rating.
+// The alt attribute of the target of the event objectn is being stored in the starNumber variable.
+//Stars variable containing the object collection referenced by the selector span# stars img.
+// Lights every star in the collection by changing the source to the bw_star2.png image.
+//Unlights every star in the collection by changing the source to the bw_star.png image.
+// Changes the value of the input box with the id of rating to the variable starNumber.
+// When the mouse moves off a star image, the lit stars will be unlit.
+// If the user clicks the star image, the selected rating should be set, so if the user moves the mouse from the star the star should still be lit.
 function lightStars(e) {
-
-      // a. Daniel has stored the rating value of each star image in the img element’ s alt attribute. Store the value of the alt attribute of the target of the event object in the starNumber variable.
+      
       var starNumber = e.target.alt;
+      var stars = document.querySelectorAll("span#stars img");
 
-
-      // b. Declare the stars variable containing the object collection referenced by the selector
-      // span# stars img.
-      var stars = document.querySelectorAll("span#stars");
-
-      // c. Loop through the stars collection with an index ranging from 0 up to less than the value of the starNumber variable. Light every star in the collection by changing the src attribute of the star image to the bw_star2.png image file.
-      for (var i = 0; i < starNumber; i++) {
-            starNumber[i].src = "bw_star2.png";
+     for (var i = 0; i < starNumber; i++) {
+            stars[i].src = "bw_star2.png";
       }
 
-      // d. After the for loop, create another loop that loops through the stars collection with the index ranging from the value of the starNumber variable to less than 5. Unlight every star in this col - lection by changing the src attribute of the star image to the bw_star.png image file.
-      for (var i = 0; i < 5; i++) {
-            starNumber[i].src = "bw_star.png";
+      for (var i = starNumber; i < 5; i++) {
+            stars[i].src = "bw_star.png";
       }
 
-      // e. Change the value of the input box with the id attribute“ rating” to starNumber stars, where starNumber is the value of the starNumber variable.
       document.getElementById("rating").value = starNumber + "stars";
-
-      // f. When the mouse pointer moves off a star image, the lit stars should be unlit.Add an event listener to the target of the event object that runs the turnOffStars() function in response to the mouseleave event.
-      e.target.addEventListener = turnOffStars();
-
-      // g. If the user clicks the star image, the selected rating should be set, which means moving the mouse pointer off the star should not remove the rating. Add an event listener for the target of the event object that runs an anonymous function removing the turnOffStars() function from the mouseleave event.
-      document.addEventListener
-
+      e.target.addEventListener("mouseleave", turnOffStars);
+      e.target.addEventListener("click",
+       function(){
+            e.target.removeEventListener("mouseleave", turnOffStars);
+      }
+      );     
 }
 
+// This function is to unlight the stars when the user moves the mouse pointer off the stars.
+//Stars variable containing the object collection referenced by the selector span# stars img.
+//Unlights every star in the collection by changing the source to the bw_star.png image.
+// Changes the value of the rating input box to an empty text string. 
+function turnOffStars() {
+      var stars = document.querySelectorAll("span#stars img");
+      for (var i= 0; i < 5; i++) {
+            stars[i].src = "bw_star.png";
+      }
+      document.getElementById("rating").value = "";
+}
+
+// The updatecount() function keeps a total of the number of characters that the user has typed into the comment text area box.
+// The commentText variable references the value stored in the comment text area box.
+// The charount variable is set to the countCharacters function with commentText as the paramters.
+//  The wordcountbox references the wordCount input box.
+// The wordCount input box is set to the charCount variable plus /1000. 
+// If charCount is greater than 1000, then the style of the wordCount input box will be a white font on a red background, but if its less then 1000 then the the wordCount input box will be black font on a white background.
+function updateCount(){
+      var commentText = document.getElementById("comment").value;
+      var charCount = countCharacters(commentText);
+      var wordCountBox = document.getElementById("wordCount");
+      wordCountBox.value = charCount + "/1000";
+      if (charCount > 1000) {
+            wordCountBox.style.color = "white";
+            wordCountBox.style.backgroundColor = "red";
+      } else {
+            wordCountBox.style.color = "black";
+            wordCountBox.style.backgroundColor = "white";
+      }
+}
 
 function countCharacters(textStr) {
       var commentregx = /\s/g;
